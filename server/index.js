6d8ea44441851177d0dd9a05948c6b322b3ffd9e
Fajr <fajri.illahi@medicaboo.com>
2021-03-22 19:34:31 +0700

    all data

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';//framework for routes
+import bodyParser from 'body-parser'; // for post request
+import mongoose from 'mongoose'; // for create model
+import cors from 'cors';//cross origin request
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://admin:root@cluster0.sr3lf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
