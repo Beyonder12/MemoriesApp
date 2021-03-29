c458414354909bb84c0c04e373a75e5ef10c4aae
Fajr <fajri.illahi@medicaboo.com>
2021-03-29 09:16:17 +0700

    title editing of form and clear function for form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -33,13 +33,14 @@ const Form = ({ currentId, setCurrentId }) => {
     }
 
     const clear = () => {
-
+        setCurrentId(null);
+        setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: ''});
     }
 
     return (
         <Paper className={classes.paper}>
             <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-            <Typography variant="h6">Creating a Memory</Typography>
+            <Typography variant="h6"> {currentId ? 'Editing' : 'Creating' } Creating a Memory</Typography>
             <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/>
             <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}/>
             <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })}/>
@@ -49,7 +50,7 @@ const Form = ({ currentId, setCurrentId }) => {
             <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
             </form>
 
-        </Paper>
+        </Paper> 
     );
 }
 
